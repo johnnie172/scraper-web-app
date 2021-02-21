20412e26510425528ef21eed8683f221af25e100
Johnathan Maytliss <jmpstillsart@gmail.com>
2021-02-21 17:19:08 +0200

    Changing smtp to use SSL

--- ksp_scraper/email_utilities.py
+++ ksp_scraper/email_utilities.py
@@ -10,33 +10,59 @@ logger = logging.getLogger(__name__)
 _smtp_client = None
 
 
-def get_smtp_service():
+def test_conn_open(_smpt_client):
+
     global _smtp_client
-    if _smtp_client is None:
-        _smtp_client = smtplib.SMTP(host=smtp_config.SMTP_HOST, port=587)
-        _smtp_client.starttls()
+    logger.debug("testing connection to smpt.")
+
+    try:
+        status = _smpt_client.noop()[0]
+        logger.debug(f'smpt status: {status}')
+    except:  # smtplib.SMTPServerDisconnected
+        status = -1
+    return True if status == 250 else False
+
+
+def get_smtp_service():
+
+    global _smtp_client
+
+    if not test_conn_open(_smtp_client):
+        logger.debug(f'creating _smtp_client')
+
+        _smtp_client = smtplib.SMTP_SSL(host=smtp_config.SMTP_HOST, port=465)
+        _smtp_client.ehlo()
         _smtp_client.login(smtp_config.SMTP_ADDRESS, smtp_config.SMTP_PASSWORD)
         logger.debug(f'_smtp_client is created')
-    return _smtp_client
+
+        return _smtp_client
+    else:
+        return False
+
 
 def quit_smtp_service():
+    logger.debug(f'Quiting _smtp_client!')
     _smtp_client.quit()
-
-
+    logger.debug(f'{_smtp_client}')
 
 
 def send_target_price_mail(email, item_uin):
+
     msg = MIMEMultipart()
     message = f'The item that you wanted is now at the target price, item link: {consts.URL_TO_ADD_UIN + item_uin}.'
     msg['From'] = smtp_config.SMTP_ADDRESS
     msg['To'] = email
     msg['Subject'] = consts.EMAIL_MESSAGE_TITLE
     msg.attach(MIMEText(message, 'plain'))
-    get_smtp_service().send_message(msg)
-    logger.debug(f'Sending email to: {email}, about target price to uin:{item_uin}.')
-    quit_smtp_service()
+    _smtp_client = get_smtp_service()
 
-    del msg
+    if _smtp_client:
+        _smtp_client.send_message(msg)
+        logger.debug(f'Sending email to: {email}, about target price to uin:{item_uin}.')
+        del msg
+    else:
+
+        return False
 
 
 def send_out_of_stock_mail(email, item_title):
@@ -46,8 +72,12 @@ def send_out_of_stock_mail(email, item_title):
     msg['To'] = email
     msg['Subject'] = consts.EMAIL_MESSAGE_TITLE
     msg.attach(MIMEText(message, 'plain'))
-    get_smtp_service().send_message(msg)
-    logger.debug(f'Sending email to: {email}, about out of stock item to item:{item_title}.')
-    quit_smtp_service()
+    _smtp_client = get_smtp_service()
 
-    del msg
+    if _smtp_client:
+        _smtp_client.send_message(msg)
+        logger.debug(f'Sending email to: {email}, about out of stock item to item:{item_title}.')
+        del msg
+    else:
+
+        return False
